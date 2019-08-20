ddee8221a0b510ef34b9c31c7905b9deacebd705
Bagus <25834188+contactjavas@users.noreply.github.com>
2019-08-20 23:05:41 +0700

    phpcbf work

--- responsive-youtube-vimeo-popup.php
+++ responsive-youtube-vimeo-popup.php
@@ -7,70 +7,95 @@
  * Author: David Vongries
  * Author URI: https://mapsteps.com
  * Text Domain: wp-video-popup
+ *
+ * @package WP_Video_Popup
  */
 
-// exit if plugin is accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+defined( 'ABSPATH' ) || die( "Can't access directly" );
 
-// Plugin constants
+// Plugin constants.
 define( 'WP_VIDEO_POPUP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WP_VIDEO_POPUP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-// Textdomain
-add_action( 'plugins_loaded', 'wp_video_popup_textdomain' );
+/**
+ * Load text domain
+ *
+ * @return void
+ */
 function wp_video_popup_textdomain() {
-	load_plugin_textdomain( 'wp-video-popup', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' ); 
+	load_plugin_textdomain( 'wp-video-popup', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 }
+add_action( 'plugins_loaded', 'wp_video_popup_textdomain' );
 
-// Clean up behind us
-if( get_option( '_site_transient_disable-ryv-notice' ) ) {
+// Clean up behind us.
+if ( get_option( '_site_transient_disable-ryv-notice' ) ) {
 	delete_option( '_site_transient_disable-ryv-notice' );
 }
 
-// PAnD
+// PAnD (Persist Admin notice Dismissals).
 require_once WP_VIDEO_POPUP_PLUGIN_DIR . 'assets/pand/pand.php';
 
+/**
+ * Pro ad
+ *
+ * @return void
+ */
 function wp_video_popup_pro_ad() {
-
-	if ( ! PAnD::is_admin_notice_active( 'wp-video-popup-pro-ad-forever' ) ) return;
+	if ( ! PAnD::is_admin_notice_active( 'wp-video-popup-pro-ad-forever' ) ) {
+		return;
+	}
 
 	?>
 
 	<div data-dismissible="wp-video-popup-pro-ad-forever" class="notice notice-info is-dismissible">
-		<p><?php _e( 'WP Video Popup PRO is now available! <strong>Save 30%</strong> and download <a href="https://wp-video-popup.com/pricing/?utm_source=repository&utm_medium=admin_notice&utm_campaign=wp_video_popup" target="_blank"><strong>WP Video Popup PRO</strong></a> today.', 'wp-video-popup' ); ?></p>
+		<p>
+			<?php
+			// phpcs:ignore -- is ok.
+			_e( 'WP Video Popup PRO is now available! <strong>Save 30%</strong> and download <a href="https://wp-video-popup.com/pricing/?utm_source=repository&utm_medium=admin_notice&utm_campaign=wp_video_popup" target="_blank"><strong>WP Video Popup PRO</strong></a> today.', 'wp-video-popup' ); 
+			?>
+		</p>
 	</div>
 
 	<?php
 
 }
-add_action( 'admin_init', array( 'PAnD', 'init' ) );
 add_action( 'admin_notices', 'wp_video_popup_pro_ad' );
+add_action( 'admin_init', array( 'PAnD', 'init' ) );
 
-// Scripts & Styles
+/**
+ * Enqueue styles & scripts
+ *
+ * @return void
+ */
 function wp_video_popup_styles() {
-
-	// Enqueue Styles & Scripts
-	wp_register_style( 'wp-video-popup', WP_VIDEO_POPUP_PLUGIN_URL . 'assets/css/wp-video-popup.css' );
+	wp_register_style( 'wp-video-popup', WP_VIDEO_POPUP_PLUGIN_URL . 'assets/css/wp-video-popup.css', [], null );
 	wp_enqueue_style( 'wp-video-popup' );
 
 	wp_register_script( 'wp-video-popup', WP_VIDEO_POPUP_PLUGIN_URL . 'assets/js/wp-video-popup.js', array( 'jquery' ), '', true );
 	wp_enqueue_script( 'wp-video-popup' );
-
 }
 add_action( 'wp_enqueue_scripts', 'wp_video_popup_styles' );
 
-// Shortcode
+/**
+ * Popup shortcode
+ *
+ * @param array $wp_video_popup_atts The shortcode attributes.
+ * @return string wp_video_popup_output The popup markup.
+ */
 function wp_video_popup_shortcode( $wp_video_popup_atts ) {
+	$wp_video_popup_atts = shortcode_atts(
+		array(
+			'video'        => 'https://www.youtube.com/embed/YlUKcNNmywk',
+			'vimeo'        => 0,
+			'mute'         => 0,
+			'hide-related' => 0,
+			'start'        => 0,
+		),
+		$wp_video_popup_atts,
+		'wp-video-popup'
+	);
 
-	$wp_video_popup_atts = shortcode_atts( array(
-		'video'        => 'https://www.youtube.com/embed/YlUKcNNmywk',
-		'vimeo'        => 0,
-		'mute'         => 0,
-		'hide-related' => 0,
-		'start'        => 0
-	), $wp_video_popup_atts, 'wp-video-popup' );
-
-	// vars
+	// vars.
 	$video        = $wp_video_popup_atts['video'];
 	$vimeo        = $wp_video_popup_atts['vimeo'] ? 1 : 0;
 	$mute         = $wp_video_popup_atts['mute'] ? 1 : 0;
@@ -79,56 +104,56 @@ function wp_video_popup_shortcode( $wp_video_popup_atts ) {
 
 	$video_id = WP_Video_Popup_Parser::get_url_id( $video );
 
-	if( $vimeo ) {
-		$video_url = 'https://player.vimeo.com/video/'. $video_id .'?autoplay=1';
+	if ( $vimeo ) {
+		$video_url = 'https://player.vimeo.com/video/' . $video_id . '?autoplay=1';
 	} else {
-		$video_url = 'https://www.youtube.com/embed/'. $video_id .'?autoplay=1';
+		$video_url = 'https://www.youtube.com/embed/' . $video_id . '?autoplay=1';
 	}
 
 	/* URL Parameters */
 
-	// remove related videos
-	if( $hide_related && !$vimeo ) {
+	// remove related videos.
+	if ( $hide_related && ! $vimeo ) {
 		$video_url .= '&amp;rel=0';
 	}
 
-	// mute Vimeo video
-	if( $mute && $vimeo ) {
+	// mute Vimeo video.
+	if ( $mute && $vimeo ) {
 		$video_url .= '&amp;muted=1';
 	}
 
-	// mute YouTube video
-	if( $mute && !$vimeo ) {
+	// mute YouTube video.
+	if ( $mute && ! $vimeo ) {
 		$video_url .= '&amp;mute=1';
 	}
 
 	$video_url = apply_filters( 'wp_video_popup', $video_url );
 
-	// start Vimeo video at specific time
-	if( $start && $vimeo ) {
+	// start Vimeo video at specific time.
+	if ( $start && $vimeo ) {
 		$video_url .= '#t=' . $start;
 	}
 
-	// start YouTube video at specific time
-	if( $start && !$vimeo ) {
+	// start YouTube video at specific time.
+	if ( $start && ! $vimeo ) {
 		$video_url .= '&amp;start=' . $start;
 	}
 
 	/* Output */
 
-	// Initialize output
+	// Initialize output.
 	$wp_video_popup_output = '';
 
-	// Popup Wrapper
+	// Popup Wrapper.
 	$wp_video_popup_output .= '<div class="wp-video-popup-wrapper">';
 
-	// Close Icon
+	// Close Icon.
 	$wp_video_popup_output .= '<div class="wp-video-popup-close"></div>';
 
-	// Video
-	$wp_video_popup_output .= '<iframe class="wp-video-popup-video" src="" data-wp-video-popup-url="'. $video_url .'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay"></iframe>';
+	// Video.
+	$wp_video_popup_output .= '<iframe class="wp-video-popup-video" src="" data-wp-video-popup-url="' . $video_url . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay"></iframe>';
 
-	// Close Popup Wrapper
+	// Close Popup Wrapper.
 	$wp_video_popup_output .= '</div>';
 
 	return $wp_video_popup_output;
@@ -138,8 +163,8 @@ add_shortcode( 'wp-video-popup', 'wp_video_popup_shortcode' );
 
 /* Required Files */
 
-// Backwards Compatibility
+// Backwards Compatibility.
 require_once WP_VIDEO_POPUP_PLUGIN_DIR . 'inc/backwards-compatibility.php';
 
-// Helpers
+// Helpers.
 require_once WP_VIDEO_POPUP_PLUGIN_DIR . 'inc/helpers.php';
