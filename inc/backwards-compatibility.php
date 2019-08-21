7d7a7b86ab18b36fc2909705ed433a6c335f77bb
Bagus <25834188+contactjavas@users.noreply.github.com>
2019-08-21 13:39:57 +0700

    simplify output: the class names say what they do

--- inc/backwards-compatibility.php
+++ inc/backwards-compatibility.php
@@ -22,22 +22,14 @@ function ryv_popup_shortcode( $ryv_popup_atts ) {
 		'ryv-popup'
 	);
 
-	// Initialize output.
-	$ryv_output = '';
-
-	// Popup Wrapper.
-	$ryv_output .= '<div class="ryv-popup-wrapper">';
-
-	// Close Icon.
-	$ryv_output .= '<div class="ryv-popup-close"></div>';
-
-	// Video.
-	$ryv_output .= '<iframe class="ryv-popup-video" src="" data-ryv-popup-url="' . $ryv_popup_atts['video'] . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
-	// Close Popup Wrapper.
-	$ryv_output .= '</div>';
-
-	return $ryv_output;
+	return '
+	<div class="ryv-popup-wrapper">
+		<div class="ryv-popup-wrapper">
+			<div class="ryv-popup-close"></div>
+			<iframe class="ryv-popup-video" src="" data-ryv-popup-url="' . $ryv_popup_atts['video'] . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		</div>
+	</div>
+	';
 
 }
 add_shortcode( 'ryv-popup', 'ryv_popup_shortcode' );
