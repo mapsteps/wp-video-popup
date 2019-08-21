f9899814f06b40b774093056ca77dadce6d40f50
Bagus <25834188+contactjavas@users.noreply.github.com>
2019-08-21 13:38:29 +0700

    wpcs formatting

--- inc/backwards-compatibility.php
+++ inc/backwards-compatibility.php
@@ -4,30 +4,37 @@
  *
  * @package WP Video Popup
  */
- 
-// exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
 
-// Shortcode
+defined( 'ABSPATH' ) || die( "Can't access directly" );
+
+/**
+ * Popup shortcode
+ *
+ * @param array $ryv_popup_atts The shortcode attributes.
+ * @return string The popup markup.
+ */
 function ryv_popup_shortcode( $ryv_popup_atts ) {
+	$ryv_popup_atts = shortcode_atts(
+		array(
+			'video' => 'https://www.youtube.com/embed/YlUKcNNmywk',
+		),
+		$ryv_popup_atts,
+		'ryv-popup'
+	);
 
-	$ryv_popup_atts = shortcode_atts( array(
-		'video' => 'https://www.youtube.com/embed/YlUKcNNmywk',
-	), $ryv_popup_atts, 'ryv-popup' );
-
-	// Initialize output
+	// Initialize output.
 	$ryv_output = '';
 
-	// Popup Wrapper
+	// Popup Wrapper.
 	$ryv_output .= '<div class="ryv-popup-wrapper">';
 
-	// Close Icon
+	// Close Icon.
 	$ryv_output .= '<div class="ryv-popup-close"></div>';
 
-	// Video
-	$ryv_output .= '<iframe class="ryv-popup-video" src="" data-ryv-popup-url="'. $ryv_popup_atts['video'] .'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+	// Video.
+	$ryv_output .= '<iframe class="ryv-popup-video" src="" data-ryv-popup-url="' . $ryv_popup_atts['video'] . '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 
-	// Close Popup Wrapper
+	// Close Popup Wrapper.
 	$ryv_output .= '</div>';
 
 	return $ryv_output;
