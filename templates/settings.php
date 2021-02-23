af1a5409a9e96f17b93cb9528f4c2c527a175b18
mapsteps <connect@mapsteps.com>
2021-02-23 17:55:13 +0100

    added featured section

--- templates/settings.php
+++ templates/settings.php
@@ -31,14 +31,12 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 			</div>
 
-			<!--
 			<nav>
 				<ul class="tab-navigation">
 					<li class="tab-nav-menu-item settings"><a href="#settings"><?php _e( 'General', 'wp-video-popup' ); ?></a></li>
-					<li class="tab-nav-menu-item recommended"><a href="#recommended"><?php _e( 'Recommended Plugins', 'wp-video-popup' ); ?></a></li>
+					<li class="tab-nav-menu-item recommended"><a href="#recommended"><?php _e( 'PRO Features', 'wp-video-popup' ); ?></a></li>
 				</ul>
 			</nav>
-			-->
 
 		</div>
 
@@ -59,19 +57,17 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 					<?php do_settings_sections( 'wp-video-popup-settings' ); ?>
 				</div>
 
-				<div class="heatbox-cta-container is-attached">
-					<div class="heatbox-cta primary">
-						<p><?php _e( 'This feature is available in WP Video Popup PRO.', 'wp-video-popup' ); ?></p>
-						<a href="https://wp-video-popup.com/pricing/?utm_source=repository&utm_medium=settings_page&utm_campaign=wp_video_popup" class="button button-large button-primary" target="_blank">
-							<?php _e( 'Get WP Video Popup PRO - 30% OFF', 'wp-video-popup' ); ?>
-						</a>
-					</div>
+				<div class="heatbox-cta primary">
+					<p><?php _e( 'This feature is available in WP Video Popup PRO.', 'wp-video-popup' ); ?></p>
+					<a href="https://wp-video-popup.com/pricing/?utm_source=repository&utm_medium=settings_page&utm_campaign=wp_video_popup" class="button button-large button-primary" target="_blank">
+						<?php _e( 'Get WP Video Popup PRO - 30% off!', 'wp-video-popup' ); ?>
+					</a>
 				</div>
 
-				<div class="heatbox wpvp-pro-metabox">
-					<?php require __DIR__ . '/metaboxes/pro-features.php'; ?>
-				</div>
+			</div>
 
+			<div class="heatbox-admin-panel recommended-panel">
+				<?php require __DIR__ . '/metaboxes/pro-features.php'; ?>
 			</div>
 
 		</div>
@@ -84,6 +80,50 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 			?>
 
 		</div>
+
+		<div class="heatbox-divider"></div>
+
+	</div>
+
+	<div class="heatbox-container heatbox-container-wide heatbox-container-center featured-products">
+
+		<h2><?php _e( 'Check out our other, free WordPress products!', 'wp-video-popup' ); ?></h2>
+
+		<ul class="products">
+			<li class="heatbox">
+				<a href="https://wordpress.org/plugins/ultimate-dashboard/" target="_blank">
+					<img src="<?php echo esc_url( WP_VIDEO_POPUP_PLUGIN_URL ); ?>assets/img/ultimate-dashboard.jpg">
+				</a>
+				<div class="heatbox-content">
+					<h3><?php _e( 'Ultimate Dashboard', 'wp-video-popup' ); ?></h3>
+					<p class="subheadline"><?php _e( 'Fully customize your WordPress Dashboard.', 'wp-video-popup' ); ?></p>
+					<p><?php _e( 'Ultimate Dashboard is the #1 Plugin to create a Custom WordPress Dashboard for you and your clients. It also comes with Multisite Support which makes it the perfect plugin for your WaaS network.', 'wp-video-popup' ); ?></p>
+					<a href="https://wordpress.org/plugins/ultimate-dashboard/" target="_blank" class="button"><?php _e( 'View Features', 'wp-video-popup' ); ?></a>
+				</div>
+			</li>
+			<li class="heatbox">
+				<a href="https://wordpress.org/themes/page-builder-framework/" target="_blank">
+					<img src="<?php echo esc_url( WP_VIDEO_POPUP_PLUGIN_URL ); ?>assets/img/page-builder-framework.jpg">
+				</a>
+				<div class="heatbox-content">
+					<h3><?php _e( 'Page Builder Framework', 'wp-video-popup' ); ?></h3>
+					<p class="subheadline"><?php _e( 'The only Theme you\'ll ever need.', 'wp-video-popup' ); ?></p>
+					<p class="description"><?php _e( 'With its minimalistic design the Page Builder Framework theme is the perfect foundation for your next project. Build blazing fast websites with a theme that is easy to use, lightweight & highly customizable.', 'wp-video-popup' ); ?></p>
+					<a href="https://wordpress.org/themes/page-builder-framework/" target="_blank" class="button"><?php _e( 'View Features', 'wp-video-popup' ); ?></a>
+				</div>
+			</li>
+			<li class="heatbox">
+				<a href="https://wordpress.org/plugins/swift-control/" target="_blank">
+					<img src="<?php echo esc_url( WP_VIDEO_POPUP_PLUGIN_URL ); ?>assets/img/swift-control.jpg">
+				</a>
+				<div class="heatbox-content">
+					<h3><?php _e( 'WP Swift Control', 'wp-video-popup' ); ?></h3>
+					<p class="subheadline"><?php _e( 'Replace the boring WordPress Admin Bar with this!', 'wp-video-popup' ); ?></p>
+					<p><?php _e( 'Swift Control PRO is the plugin that make your clients love WordPress. It drastically improves the user experience when working with WordPress and allows you to replace the boring WordPress admin bar with your own navigation panel.', 'wp-video-popup' ); ?></p>
+					<a href="https://wordpress.org/plugins/swift-control/" target="_blank" class="button"><?php _e( 'View Features', 'wp-video-popup' ); ?></a>
+				</div>
+			</li>
+		</ul>
 
 	</div>
 
